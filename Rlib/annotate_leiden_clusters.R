9748e00eeecd80de710d3b7a48a09f6434fdc24c
Brian Haas <brianjohnhaas@gmail.com>
2022-09-04 14:40:03 -0400

    update

--- Rlib/annotate_leiden_clusters.R
+++ Rlib/annotate_leiden_clusters.R
@@ -9,9 +9,6 @@ annotate_leiden_clusters = function(fusion_data) {
     high_FAR_microH_bioinf_artifact_clusters = c(57,56,60)
 
     fusion_data = fusion_data %>%
-
-        mutate(fusion_cluster_att = ifelse(leiden %in% cosmic_like_clusters,
-                                           "cosmic-like", "NA")) %>%
         mutate(fusion_cluster_att = ifelse(leiden %in% expr_microH_RT_artifact_clusters, "expr_microH_RT_artifact?", fusion_cluster_att)) %>%
         mutate(fusion_cluster_att = ifelse(leiden %in% high_FAR_microH_bioinf_artifact_clusters, "high_FAR_microH_bioinf_artifact?", fusion_cluster_att)) %>%
         mutate(fusion_cluster_att = ifelse(leiden == cosmic_peak_enriched_cluster, "cosmic-peak-enriched", fusion_cluster_att))
